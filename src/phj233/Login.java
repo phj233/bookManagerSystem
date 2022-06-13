a2cfb271e3f6bf30fca4d8c38b70abefb8c34e5d
phj233 <2780990934@qq.com>
2022-06-14 00:20:49 +0800

    ~~

--- src/phj233/Login.java
+++ src/phj233/Login.java
@@ -1,5 +1,4 @@
 package phj233;
-
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
@@ -10,11 +9,17 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
 import java.util.Optional;
+import  java.sql.*;
 
 public class Login extends Application {
     public static void main(String[] args) {
+//        try{
+//            Class.forName("com.mysql.jdbc.Driver");
+//        } catch (SQLException se){
+//
+//        }
+
         Application.launch(args);
     }
 
@@ -22,34 +27,52 @@ public class Login extends Application {
     public void start(Stage stage) throws Exception {
         stage.setTitle("图书管理系统--管理员登录");
         stage.getIcons().add(new Image("img/icon.png"));
-        stage.setWidth(400);
-        stage.setHeight(250);
+        stage.setWidth(600);
+        stage.setHeight(400);
 
         GridPane gr =new GridPane();
         gr.setStyle("-fx-background-color:#FFF5EE");
         Label l_name =new Label("账号：");
         Label l_passwd = new Label("密码：");
+        Label l_sql = new Label("SQL地址：");
+        Label l_sql_name = new Label("SQL用户：");
+        Label l_sql_passwd = new Label("SQL密码：");
 
         TextField t_name = new TextField();
+        TextField t_sql_name = new TextField();
+        PasswordField p_sql_passwd = new PasswordField();
+        TextField t_sql = new TextField("mysql://local:22/book");
         PasswordField p_passwd = new PasswordField();
         //t_name.getProperties().put("phj233","123456");
         t_name.setUserData("phj233");
         p_passwd.setUserData("123456");
         Button login = new Button("登录");
         Button clear = new Button("清除");
+        Button connect = new Button("连接");
 
         gr.setAlignment(Pos.CENTER);
         gr.setHgap(5);
         gr.setVgap(15);
-        gr.add(l_name,0,0);
+        gr.add(l_sql_name,0,1);
+        l_sql_name.setFont(Font.font(15));
+        gr.add(t_sql_name,1,1);
+        gr.add(l_sql_passwd,2,1);
+        l_sql_passwd.setFont(Font.font(15));
+        gr.add(p_sql_passwd,3,1);
+        gr.add(l_sql,0,2);
+        l_sql.setFont(Font.font(15));
+        gr.add(t_sql,1,2);
+        gr.add(connect,2,2);
+        gr.add(l_name,0,3);
         l_name.setFont(Font.font(15));
-        gr.add(t_name,1,0);
-        gr.add(l_passwd,0,1);
+        gr.add(t_name,1,3);
+        gr.add(l_passwd,2,3);
         l_passwd.setFont(Font.font(15));
-        gr.add(p_passwd,1,1);
-        gr.add(clear,0,2);
-        gr.add(login,1,2);
+        gr.add(p_passwd,3,3);
+        gr.add(clear,2,4);
+        gr.add(login,3,4);
         GridPane.setMargin(login,new Insets(0,0,0,120));
+
 //      BorderPane root = new BorderPane();
         Scene scene = new Scene(gr,400,250);
         stage.setScene(scene);
@@ -64,6 +87,9 @@ public class Login extends Application {
             if (result.get()==ButtonType.OK){
                 Platform.exit();
             }
+        });
+        connect.setOnAction(actionEvent -> {
+                String sql_url = "jdbc:"+t_sql.getText()+"?useUnicode=true&characterEncoding=UTF-8";
         });
         clear.setOnAction(actionEvent -> {
             t_name.setText("");
--- src/phj233/Main.java
+++ src/phj233/Main.java
@@ -4,18 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-
 import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("./main.fxml")));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
